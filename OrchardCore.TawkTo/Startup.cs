5e38dd74970a18ec2c50a93d2ba9d755802079b0
Rumen Novachkov <r.novachkov@gmail.com>
2023-10-05 10:21:34 +0300

    Add project files.

---
+++ OrchardCore.TawkTo/Startup.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace OrchardCore.TawkTo
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            routes.MapAreaControllerRoute(
+                name: "Home",
+                areaName: "OrchardCore.TawkTo",
+                pattern: "Home/Index",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+        }
+    }
+}
