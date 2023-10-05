433647ec7c7512a023bde52d145413a464aa7bf5
Rumen Novachkov <r.novachkov@gmail.com>
2023-10-05 12:08:02 +0300

    Packed

--- OrchardCore.TawkTo/Manifest.cs
+++ OrchardCore.TawkTo/Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "TawkTo Customers Chat",
     Author = "Rumen Novachkov",
     Website = "https://github.com/RNovachkov/OrchardCore.TawkTo",
-    Version = "0.0.1",
+    Version = "1.0.0",
     Description = "OrchardCore.TawkTo is an unofficial OrchardCore module for TawkTo integration.",
     Category = "Communication",
     Tags = new string[] { "communication", "customer", "chat", "realtime", "client" }
